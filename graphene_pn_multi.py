337bf8fdad620046df872763077097500d2a39e2
eduardzsurka <eduardzsurka@ttk.elte.hu>
2021-11-02 10:00:16 +0100

    Deleting whitespaces

--- graphene_pn_multi.py
+++ graphene_pn_multi.py
@@ -71,7 +71,7 @@ def Color(site):
     y = site.pos[1]/H_pn
     if(y<1):
         return [1-y,0,y]
-    else: 
+    else:
         return [0,0,1]
 
 def square(pos):
@@ -89,27 +89,27 @@ def create_junction( t, on_site, mu_scattering, mu_n, mu_p, mu_S, Delta, param_s
             return - mu_scattering  * tau_z
         else:
             return - ( mu_p - (mu_p-mu_scattering)*y/H_pn ) * tau_z
-    
+
     sys = kwant.Builder(conservation_law=-tau_z, particle_hole = tau_y)
     sys[graphene.shape(square, (0,0))] =  pn_junction
     sys[graphene.neighbors()] = - t * tau_z
 
-    #kwant.plot(sys);        
+    #kwant.plot(sys);
 
     negativeL = kwant.Builder(kwant.TranslationalSymmetry([1 ,0]), conservation_law=-tau_z, particle_hole = tau_y)
     negativeL[graphene.shape(side_leads, (0,H))] = ( - mu_n ) * tau_z
     negativeL[graphene.neighbors()] = - t * tau_z
-    
+
     positiveL = kwant.Builder(kwant.TranslationalSymmetry([0 ,-np.sqrt(3)]), conservation_law=-tau_z, particle_hole = tau_y)
     positiveL[graphene.shape(square, (0,H))] = ( - mu_p ) * tau_z
     positiveL[graphene.neighbors()] = - t * tau_z
-     
+
     superL = kwant.Builder(kwant.TranslationalSymmetry([0, +np.sqrt(3)]))
     superL[graphene.shape(square, (0,0))] = ( - mu_S ) * tau_z + Delta_mat
-    superL[graphene.neighbors()] = - t * tau_z    
-        
+    superL[graphene.neighbors()] = - t * tau_z
+
     #kwant.plotter.bands(negativeL.finalized(),fig_size=(12,12),momenta=200,file="sideL_H"+str(H/2)+".png");
-   
+
     sys.attach_lead(negativeL)
     sys.attach_lead(negativeL.reversed())
     sys.attach_lead(positiveL)
@@ -117,18 +117,18 @@ def create_junction( t, on_site, mu_scattering, mu_n, mu_p, mu_S, Delta, param_s
         sys.attach_lead(superL)
 
     # SZÍNSKÁLÁT!
-    kwant.plot(sys,file="sys_"+param_string+".png",dpi=100,site_color=Color,show=True,ax = ax,site_size=0.4,lead_color="black");  
-    
-    gca = fig.gca()    
+    kwant.plot(sys,file="sys_"+param_string+".png",dpi=100,site_color=Color,show=True,ax = ax,site_size=0.4,lead_color="black");
+
+    gca = fig.gca()
     gca.set_aspect("equal")
 
     ax.plot([-2.5,-2.5+dist_nm],[-2.5,-2.5],lw=3,color="black");
-    ax.text(-2.5,-1,str(dist)+" nm")                
+    ax.text(-2.5,-1,str(dist)+" nm")
     fig.savefig(fname="sys_"+param_string+".png",)
-    
+
     return sys
 
-fig, ax = pyplot.subplots(nrows=1,ncols=1)    
+fig, ax = pyplot.subplots(nrows=1,ncols=1)
 aspect = (20+W_S)/(20+H+H_L)
 dim = 16
 if(aspect>1):
@@ -177,19 +177,19 @@ pyplot.xlim([-maxE/Delta,maxE/Delta]);
 if( supra_ok ):
     pyplot.plot([ 1, 1],Max_ys,"k--");
     pyplot.plot([-1,-1],Max_ys,"k--");
-                
+
 pyplot.title("$W_S="+str(W_S)+",\, H_L="+str(round(H_L,1))+",\, H="+str(round(H,1))+",\, H_{pn}="+str(H_pn)+
              ",$\n$\mu_{scatt.}=\mu_N="+str(int(mu_n/Delta))+"$ meV$,\, \mu_P="+str(int(mu_p/Delta))+"$ meV$,\, \Delta="+str(int(Delta/Delta))+"$ meV$"+
-             ",$\n$H_{pn}/\lambda_F^N="+str(int(H_pn_m*1e9))+"/"+str(int(lambda_F*1e9))+"="+"{:.3f}".format(d_pn)+             
-#              ",$\n$H_{pn}/\lambda_F="+"{:.1f}".format(d_pn)+             
-             ",\,\Delta/E_{Th}\propto (H+H_L)/\\xi_0="+"{:.3f}".format( (H+H_L)*lattice_c*s_f / (xi_0) )+ #Delta/E_Th)+     
+             ",$\n$H_{pn}/\lambda_F^N="+str(int(H_pn_m*1e9))+"/"+str(int(lambda_F*1e9))+"="+"{:.3f}".format(d_pn)+
+#              ",$\n$H_{pn}/\lambda_F="+"{:.1f}".format(d_pn)+
+             ",\,\Delta/E_{Th}\propto (H+H_L)/\\xi_0="+"{:.3f}".format( (H+H_L)*lattice_c*s_f / (xi_0) )+ #Delta/E_Th)+
              "$",fontsize=18);
 
 pyplot.xlabel("$E\;$[meV]",fontsize=18)
 pyplot.ylabel("$T$",fontsize=18)
 pyplot.legend(["$T_{12}$","$T_{12A}$","$T_{13}$","$T_{13A}$","$\mu_P$","$\mu_N$","$\Delta$"],fontsize=18);
 pyplot.xticks(fontsize=18); pyplot.yticks(fontsize=18);
-pyplot.savefig("T_"+"_".join(sys.argv[1:])+".png");  
+pyplot.savefig("T_"+"_".join(sys.argv[1:])+".png");
 
 with open("./T_"+"_".join(sys.argv[1:])+".npy",'wb') as file:
     np.save(file,[energy,T_11,T_11A,T_12,T_12A,T_13,T_13A])
